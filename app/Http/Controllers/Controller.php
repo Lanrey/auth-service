4bfe4c98256c7261234eb8bf3ef85c65d4d099a9
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-27 09:08:11 +0100

    Added fractal response to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use App\Http\Response\FractalResponse;
+use League\Fractal\TransformerAbstract;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    // Child Method for all Controllers
+     /**
+     * @var FractalResponse
+     */
+    private $fractal;
 
+    public function __construct(FractalResponse $fractal)
+    {
+        $this->fractal = $fractal;
+        $this->fractal->parseIncludes();
+    }
+
+    /**
+     * @param $data
+     * @param TransformerAbstract $transformer
+     * @param null $resourceKey
+     * @return array
+     */
+    public function item($data, TransformerAbstract $transformer, $resourceKey = null)
+    {
+        return $this->fractal->item($data, $transformer, $resourceKey);
+    }
+
+    /**
+     * @param $data
+     * @param TransformerAbstract $transformer
+     * @param null $resourceKey
+     * @return array
+     */
+    public function collection($data, TransformerAbstract $transformer, $resourceKey = null)
+    {
+        return $this->fractal->collection($data, $transformer, $resourceKey);
+    }
+    
+    // Generate Token Method for all Controllers
     protected function respondWithToken($token)
     {
         return response()->json([
