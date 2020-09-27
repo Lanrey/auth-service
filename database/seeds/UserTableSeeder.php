3e68bfa0a3f05ee393f0b6a30af264defe96addd
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-27 09:17:26 +0100

    Refactored user table seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,53 +13,6 @@ class UserTableSeeder extends Seeder
 
    public function run()
    {
-     DB::table('users')->insert([
-       'name' => 'Bode george',
-       'email' => 'bodegeorge@gmail.com',
-       'password' => 'frahnn###',
-       'created_at' => Carbon::now(),
-       'updated_at' => Carbon::now()
-     ]);
-
-     DB::table('users')->insert([
-      'name' => 'georgegraham',
-      'email' => 'bodegeorge2@gmail.com',
-      'password' => 'frahnnkgk###',
-      'created_at' => Carbon::now(),
-      'updated_at' => Carbon::now()
-    ]);
-
-    DB::table('users')->insert([
-      'name' => 'Bodeigigeorge',
-      'email' => 'bodegeorge4@gmail.com',
-      'password' => 'fratnn###',
-      'created_at' => Carbon::now(),
-      'updated_at' => Carbon::now()
-    ]);
-
-    DB::table('users')->insert([
-      'name' => 'Bodrgeorge',
-      'email' => 'bodegeorrge@gmail.com',
-      'password' => 'frahntt###',
-      'created_at' => Carbon::now(),
-      'updated_at' => Carbon::now()
-    ]);
-
-    DB::table('users')->insert([
-      'name' => 'Bodtgeorge',
-      'email' => 'bodtegeorge@gmail.com',
-      'password' => 'ftrahnn###',
-      'created_at' => Carbon::now(),
-      'updated_at' => Carbon::now()
-    ]);
-
-    DB::table('users')->insert([
-      'name' => 'Bodetgeorge',
-      'email' => 'bodetgeorge@gmail.com',
-      'password' => 'frtahnn###',
-      'created_at' => Carbon::now(),
-      'updated_at' => Carbon::now()
-    ]);
-
+     $users = factory(App/User::class, 50)->create();
    }
 }
