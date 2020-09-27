5115f49c756fafd74a47cf9ddce300828a8b2f4c
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-27 08:19:44 +0100

    Added handler tests

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -4,36 +4,105 @@ use \Mockery as m;
 use App\Exceptions\Handler;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class HandlerTest extends TestCase
 {
-  /** @test **/
+    /** @test **/
+    public function it_responds_with_html_when_json_is_not_accepted()
+    {
+        // Make the mock a partial, we only want to mock the `isDebugMode` method
+        $subject = m::mock(Handler::class)->makePartial();
+        $subject->shouldNotReceive('isDebugMode');
 
-  public function it_responds_with_html_when_json_is_not_accepted()
-  {
-    // Make the mock a partial, you only want to mock the `isDebugMode` method
+        // Mock the interaction with the Request
+        $request = m::mock(Request::class);
+        $request->shouldReceive('wantsJson')->andReturn(false);
 
-    $subject = m::mock(Handler::class)->makePartial();
-    $subject->shouldNotReceive('isDebugMode');
+        // Mock the interaction with the exception
+        $exception = m::mock(\Exception::class, ['Error!']);
+        $exception->shouldNotReceive('getStatusCode');
+        $exception->shouldNotReceive('getTrace');
+        $exception->shouldNotReceive('getMessage');
 
-    // Mock the interaction with the Request
-    $request = m::mock(Request::class);
-    $request->shouldReceive('wantsJson')->andReturn(false);
+        // Call the method under test, this is not a mocked method.
+        $result = $subject->render($request, $exception);
 
-    // Mock the interaction with the exception
+        // Assert that `render` does not return a JsonResponse
+        $this->assertNotInstanceOf(JsonResponse::class, $result);
+    }
 
-    $exception = m::mock(\Exception::class, ['Error!']);
-    $exception->shouldNotReceive('getStatusCode');
-    $exception->shouldNotReceive('getTrace');
-    $exception->shouldNotReceive('getMessage');
+    /** @test */
+    public function it_responds_with_json_for_json_consumers()
+    {
+        $subject = m::mock(Handler::class)->makePartial();
+        $subject
+            ->shouldReceive('isDebugMode')
+            ->andReturn(false);
 
-    // Call the method under test
+        $request = m::mock(Request::class);
+        $request
+            ->shouldReceive('wantsJson')
+            ->andReturn(true);
 
-    $result = $subject->render($request, $exception);
+        $exception = m::mock(\Exception::class, ['Doh!']);
+        $exception
+            ->shouldReceive('getMessage')
+            ->andReturn('Doh!');
 
-    // Assert that `render` does not return a JsonResponse
+        /** @var JsonResponse $result */
+        $result = $subject->render($request, $exception);
+        $data = $result->getData();
 
-    $this->assertNotInstanceOf(JsonResponse::class, $result);
-  }
+        $this->assertInstanceOf(JsonResponse::class, $result);
+        $this->assertObjectHasAttribute('error', $data);
+        $this->assertAttributeEquals('Doh!', 'message', $data->error);
+        $this->assertAttributeEquals(400, 'status', $data->error);
+    }
+
+    /** @test */
+    public function it_provides_json_responses_for_http_exceptions()
+    {
+        $subject = m::mock(Handler::class)->makePartial();
+        $subject
+            ->shouldReceive('isDebugMode')
+            ->andReturn(false);
+
+        $request = m::mock(Request::class);
+        $request->shouldReceive('wantsJson')->andReturn(true);
+
+        $examples = [
+            [
+                'mock' => NotFoundHttpException::class,
+                'status' => 404,
+                'message' => 'Not Found'
+            ],
+            [
+                'mock' => AccessDeniedHttpException::class,
+                'status' => 403,
+                'message' => 'Forbidden'
+            ],
+            [
+                'mock' => ModelNotFoundException::class,
+                'status' => 404,
+                'message' => 'Not Found'
+            ]
+        ];
+
+        foreach ($examples as $e) {
+            $exception = m::mock($e['mock']);
+            $exception->shouldReceive('getMessage')->andReturn(null);
+            $exception->shouldReceive('getStatusCode')->andReturn($e['status']);
+
+            /** @var JsonResponse $result */
+            $result = $subject->render($request, $exception);
+            $data = $result->getData();
+
+            $this->assertEquals($e['status'], $result->getStatusCode());
+            $this->assertEquals($e['message'], $data->error->message);
+            $this->assertEquals($e['status'], $data->error->status);
+        }
+    }
 }
