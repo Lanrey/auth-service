3171567248982e0705f0da8d1b6f3c5c350b213d
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-27 09:02:44 +0100

    Added FractalServiceprovider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -96,6 +96,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\EventServiceProvider::class);
 
    $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+   $app->register(\App\Providers\FractalServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
