0e498853612ea4bf4c9b070227045cf1f90ba0e9
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-27 08:48:01 +0100

    Added a transformer object for consistent responses

--- Transformer/UserTransformer.php
+++ Transformer/UserTransformer.php
@@ -3,9 +3,29 @@
 
 namespace App\Transformer;
 
+
+use App\User;
 use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
-  
+
+  /**
+   * Transform a User model into an array
+   * 
+   * @param User $user
+   * @return array
+   */
+
+   public function transform(User $user)
+   {
+     return [
+       'id'      => $user->id,
+       'name'    => $user->name,
+       'email'   => $user->email,
+       'created' => $user->created_at->toIso8601String(),
+       'updated' => $user->udpated_at->toIso8601String(),
+     ];
+   }
+
 }
