1796adc1ef5b278a814d95072bb4bf9f65499237
Akinsulere Olusola <akinsulereolusola@gmail.com>
2020-09-28 03:50:54 +0100

    Added User Controller Test

--- tests/app/Http/controllers/UserControllerTest.php
+++ tests/app/Http/controllers/UserControllerTest.php
@@ -14,9 +14,12 @@ class UserControllerTest extends TestCase
   public function all_users_status_code_should_be_200()
   {
     $user = factory('App\User')->make();
+    
+   // $token = 'Bearer ' . json_decode($response->getContent())->token;
 
     $this->actingAs($user)
-         ->get('/api/user')->seeStatusCode(200);
+    //print($auth_user);
+         ->get('/api/users', ['HTTP_Authorization' => $token])->seeStatusCode(200);
     //$this->get('/api/users')->seeStatusCode(200);
   }
 
@@ -24,34 +27,60 @@ class UserControllerTest extends TestCase
   public function single_user_status_code_should_be_200()
   {
 
-    $user = factory('App\User')->create();
+    $user = factory('App\User')->make();
 
     $this->actingAs($user)
-         ->get('/api/user')->seeStatusCode(200);
-    $this->get('/api/user/1')->seeStatusCode(200);
+         ->get('/api/user/1')->seeStatusCode(200);
+  
   }
 
   /** @test **/
   public function authenticated_user_status_code_should_be_200()
   {
-    $this->get('/api/auth-user-profile')->seeStatusCode(200);
+
+    $user = factory('App\User')->create();
+
+    $this->actingAs($user)
+         ->get('/api/user/1')->seeStatusCode(200);
   }
 
   /** @test **/
   public function show_should_return_a_valid_user()
   {
-    $this->markTestIncomplete('Pending test');
+    
+  }
+
+  /** @test **/
+
+  public function users_should_return_a_collection_of_records()
+  {
+        $users = factory('App\User', 2)->make();
+
+        $this->get('/api/users');
+
+        $content = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('data', $content);
+
+        foreach ($users as $user) {
+            $this->seeJson([
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->description,
+                'created' => $user->created_at->toIso8601String(),
+                'updated' => $user->updated_at->toIso8601String(),
+            ]);
+        }
   }
 
   /** @test **/
   public function show_should_fail_when_the_user_id_does_not_exist()
   {
-    $this->markTestIncomplete('Pending test');
+    
   }
 
   /** @test **/
   public function show_route_should_not_match_an_invalid_route()
   {
-    $this->markTestIncomplete('Pending test');
+    
   }
 }
